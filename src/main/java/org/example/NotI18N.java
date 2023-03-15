6b9737fb4926b0a8259095ac109eb5766cdba0af
Renaldo Swanepoel <renaldo@ibureau.services>
2023-03-15 10:02:29 +0200

    Sample codes with ResourceBundles

---
+++ src/main/java/org/example/NotI18N.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class NotI18N {
+    public static void main(String[] args) {
+
+        System.out.println("Hello.");
+        System.out.println("How are you?");
+        System.out.println("Goodbye.");
+    }
+}
